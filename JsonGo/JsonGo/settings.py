0ee7f27f025680e434f917d49b35e716feff98ac
ProgCat <progcat@protonmail.com>
2021-12-23 11:58:36 +0800

    I have no idea why statics not working

--- JsonGo/JsonGo/settings.py
+++ JsonGo/JsonGo/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -115,8 +114,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+import os
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, '/static/'),
+    os.path.join(BASE_DIR, '/tacoeats/static/')
+        )
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
