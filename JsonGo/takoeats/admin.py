3c2d6d2be9f7a21afd2382773cfd8bccb8e17376
ProgCat <progcat@protonmail.com>
2021-12-25 23:48:05 +0800

    added all tables to admin page

--- JsonGo/takoeats/admin.py
+++ JsonGo/takoeats/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import User, Shop, Item, ItemCategory, Order, OrderItem
+
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Shop)
+admin.site.register(Item)
+admin.site.register(ItemCategory)
+admin.site.register(Order)
+admin.site.register(OrderItem)
